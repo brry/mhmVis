390ce72e713d6ebbf17fd4d1b8f9e78faf8f016c
brry <berryboessenkool@hotmail.com>
2017-03-08 21:35:35 +0100

    read_asc: initial release. vis_asc completely rewritten accordingly.

---
+++ R/read_asc.R
@@ -0,0 +1,65 @@
+#' Read asc files
+#'
+#' Read mHM-typical ASC raster files and create a full set of (projected) coordinates
+#'
+#' @return invisible list with matrices for asc, x and y,
+#' as well as character strings for the filename (file and name) and the projection (proj).
+#' x and y are original coordinates, unless \code{proj != NA}, when they are transformed to lat-long degrees.
+#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Feb-March 2017
+#' @seealso \code{\link{vis_asc}} for plotting the result
+#' @keywords file
+#' @importFrom OSMscale projectPoints pll
+#' @importFrom utils choose.dir tail
+#' @importFrom tools file_path_sans_ext
+#' @export
+#' @examples
+#' # to be added
+#'
+#' @param file      Name of file. DEFAULT: \code{\link{file.choose}()}
+#' @param proj      Current projection string or NA.
+#'                  If not NA, coordinates will be converted to lat-long coordinates.
+#'                  If \code{proj} is a number, e.g. 3035,
+#'                  it will be replaced with the online epsg proj4 from
+#'                  \url{http://www.spatialreference.org/ref/epsg/3035/}.
+#'                  Note that setting code{proj} may increase computing time a lot.
+#'                  \code{proj} is passed as the \code{from} argument to
+#'                  \code{OSMscale::\link[OSMscale]{projectPoints}}.
+#'                  DEFAULT: NA
+#' @param \dots     Further arguments passed to \code{\link{read.table}}
+#'
+read_asc <- function(
+ file=file.choose(),
+ proj=NA,
+ ...)
+{
+  # get meta information
+  meta1 <- read.table(file, nrows=6, as.is=TRUE)
+  meta <- data.frame(t(meta1[,2]))
+  colnames(meta) <- meta1[,1]
+  rm(meta1)
+  NAS <- meta$NODATA_value
+  y <- seq(from=meta$yllcorner, by=meta$cellsize, length.out=meta$nrows)
+  x <- seq(from=meta$xllcorner, by=meta$cellsize, length.out=meta$ncols)
+  xy <- expand.grid(x,y, KEEP.OUT.ATTRS=FALSE)
+  colnames(xy) <- c("x","y")
+  rm(x,y)
+  # project coordinates:
+  if(!is.na(proj))
+    {
+    # from mHM/pre-proc/create_latlon.py line 115
+    # http://www.spatialreference.org/ref/epsg/31463/
+    if(is.numeric(proj)) proj <- readLines(paste0("http://www.spatialreference.org/ref/epsg/",
+                                                proj,"/proj4/"), warn=FALSE)
+    if(!is.character(proj)) stop("proj must be a character, not a ",class(proj),".\n  ",proj)
+    xy <- OSMscale::projectPoints(lat=xy$y, long=xy$x, from=proj, to=OSMscale::pll() )
+    }
+  # read asc file itself:
+  asc <- read.table(file=file, skip=6, na.strings=NAS, ...)
+  asc <- t(apply(asc,  2, rev))
+  asc <- unname(asc)
+  # output:
+  x <- matrix(xy$x, nrow=nrow(asc), ncol=ncol(asc))
+  y <- matrix(xy$y, nrow=nrow(asc), ncol=ncol(asc))
+  name <- tools::file_path_sans_ext(basename(file))
+  return(invisible(list(asc=asc, x=x, y=y, file=file, name=name, proj=proj)))
+}
--- R/vis_asc.R
+++ R/vis_asc.R
@@ -1,42 +1,86 @@
 #' Visualize asc files
+#' plot mHM-typical ASC raster files
 #'
-#' Visualize asc files (commonly used in mHM)
-#'
-#' @return asc matrix
-#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Feb 2017
-#' @seealso \code{\link{vis_dem}}
+#' @return None, used for plotting
+#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Feb-March 2017
+#' @seealso \code{\link{read_asc}}, \code{\link{vis_dem}}
 #' @keywords hplot color spatial
-#' @importFrom berryFunctions colPointsLegend seqPal
+#' @importFrom berryFunctions colPoints colPointsLegend seqPal owa almost.equal
 #' @importFrom graphics par image
 #' @export
 #' @examples
 #' # to be added
 #'
-#' @param file    Name of file. DEFAULT: \code{\link{file.choose}()}
+#' @param asc     List returned by \code{\link{read_asc}} or character string
+#'                giving a filename (passed to read_asc). DEFAULT: file.choose()
+#' @param proj    Projection passed to \code{\link{read_asc}} if \code{asc}
+#'                is not a list. DEFAULT: NA
 #' @param pdf,png Save output to disc? See \code{\link{pdf_png}}.
 #'                DEFAULT: PDF=FALSE, png=TRUE
-#' @param col     Color palette. DEFAULT:
-#'                \code{berryFunctions::\link[berryFunctions]{seqPal}(100)}
+#' @param outfile Output filename without pdf/png extension. DEFAULT: asc$file
+#' @param pdfargs List of arguments passed to \code{\link{pdf_png}}.
+#' @param add     Logical: add to existing plot? DEFAULT: FALSE
+#' @param asp     Plot aspect ratio, see \code{\link{plot.window}}. DEFAULT: 1
+#' @param mar,mgp Plot margins and margin placement, see \code{\link{par}}.
+#'                Margins are increased if missing and \code{asc$proj} != NA.
+#'                DEFAULT: c(0,0,0,0), c(3,0.7,0)
+#' @param col     Color palette. DEFAULT: \code{berryFunctions::\link{seqPal}(100)}
 #' @param bg      Graph background color. DEFAULT: "grey"
-#' @param \dots   Further arguments passed to
-#'                \code{berryFunctions::\link[berryFunctions]{colPointsLegend}}
+#' @param pch,cex Point type + size for colPoints if \code{asc$proj} != NA. DEFAULT: 15, 0.7
+#' @param legend  Logical: add \code{\link{colPointsLegend}}? DEFAULT: TRUE
+#' @param title   Character: Legend title. DEFAULT: asc$name (obtained from filename)
+#' @param legargs List of arguments passed to \code{\link{colPointsLegend}}.
+#' @param \dots   Arguments passed to \code{graphics::\link{image}} or
+#'                \code{berryFunctions::\link[berryFunctions]{colPoints}}
+#'                (if \code{proj} != NA).
 #'
 vis_asc <- function(
- file=file.choose(),
+ asc=file.choose(),
+ proj=NA,
  pdf=FALSE,
  png=TRUE,
+ outfile=asc$file,
+ pdfargs=NULL,
+ add=FALSE,
+ asp=1,
+ mar=c(0,0,0,0),
+ mgp=c(3,0.7,0),
  col=seqPal(100),
  bg="grey",
+ pch=15,
+ cex=0.7,
+ legend=TRUE,
+ title=asc$name,
+ legargs=NULL,
  ...)
 {
-  asc <- read.table(file, skip=6, na.strings=-9999)
-  # Plot prep
-  pdf_png(file, pdf, png)
-  # plot:
-  par(mar=rep(0,4), bg=bg)
-  image(t(apply(asc, 2, rev)), col=col, asp=1)
-  colPointsLegend(unlist(asc), colors=col, ...)
-  if(pdf|png) dev.off()
-  # output:
-  return(invisible(asc))
+# read file if asc is not an appropriate list
+if(!is.list(asc)) asc <- read_asc(asc,proj)
+# check list elements:
+elems <- c("asc","x","y","file","name","proj")
+isin <-  elems %in% names(asc)
+if(!all(isin)) stop("asc does not contain ", toString(elems[!isin]))
+# default margins
+if(!is.na(asc$proj) & missing(mar)) mar <- c(2.1,2.7,0.5,0.5)
+# saving plot setup
+if(!add)
+  {
+  pdfdefaults <- list(file=outfile, pdf=pdf, png=png)
+  do.call(pdf_png, berryFunctions::owa(pdfdefaults, pdfargs))
+  par(mar=mar, mgp=mgp, bg=bg)
+  }
+# plot asc:
+axes <- !all(almost.equal(mar,0))
+if(is.na(asc$proj))
+ graphics::image(asc$asc, col=col, asp=asp, add=add, axes=axes, ...) else
+berryFunctions::colPoints(as.vector(asc$x), as.vector(asc$y), as.vector(asc$asc),
+                          col=col, pch=pch, cex=cex, add=add, asp=asp,
+                          axes=axes, legend=FALSE, ...)
+# Add legend:
+if(legend)
+  {
+  legdefs <- list(z=unlist(asc$asc), colors=col, bg="transparent", title=title)
+  do.call(berryFunctions::colPointsLegend, berryFunctions::owa(legdefs, legargs))
+  }
+if(pdf|png) if(!add) dev.off()
 }
