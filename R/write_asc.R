5e0f2016eeb8a079edd8ed02c5ce1f749c372686
brry <berryboessenkool@hotmail.com>
2017-03-09 16:49:58 +0100

    write_asc: initial release

---
+++ R/write_asc.R
@@ -0,0 +1,36 @@
+#' Write asc files
+#'
+#' Write asc file to disc (useful after changing the asc$asc element).
+#'
+#' @return Nothing
+#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, March 2017
+#' @seealso \code{\link{read_asc}}
+#' @keywords file
+#' @importFrom berryFunctions newFilename
+#' @importFrom utils write.table
+#' @export
+#' @examples
+#' # to be added
+#'
+#' @param asc     List returned by \code{\link{read_asc}}.
+#' @param file    Filename. DEFAULT: \code{berryFunctions::\link{newFilename}(asc$file)}
+#' @param \dots   Further arguments passed to \code{\link{write.table}}.
+#'
+write_asc <- function(
+ asc,
+ file=newFilename(asc$file),
+  ...)
+{
+# check list elements:
+elems <- c("asc","file","meta", "NAS")
+isin <-  elems %in% names(asc)
+if(!all(isin)) stop("asc list does not contain the element ", toString(elems[!isin]))
+# Transpose back to original arrangement:
+out <- apply(t(asc$asc), 2, rev)
+# replace NA with appropriate value:
+out <- replace(out, is.na(out), asc$NAS)
+# Write to disc:
+cat(asc$meta, file=file, sep="\n")
+write.table(out, file=file, append=TRUE, row.names=FALSE, col.names=FALSE, quote=FALSE, ...)
+
+}
