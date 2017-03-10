f0429803622bca2262e11bc162cfbee3fab1aecd
brry <berryboessenkool@hotmail.com>
2017-03-10 14:08:20 +0100

    vis_dem: now calls vis_river

--- R/vis_dem.R
+++ R/vis_dem.R
@@ -8,31 +8,36 @@
 #' @seealso \code{\link{read_dem}}, \code{\link{vis_asc}} for plotting any asc file
 #' @keywords hplot color spatial
 #' @importFrom grDevices terrain.colors
+#' @importFrom berryFunctions seqPal
 #' @export
 #' @examples
 #' # to be added
 #'
 #' @param dem     List returned by \code{\link{read_dem}} or character string
 #'                giving a directory (passed to read_dem).
-#' @param proj    projection passed to \code{\link{read_asc}}. DEFAULT: NA
+#' @param proj    Projection passed to \code{\link{read_asc}}. DEFAULT: NA
 #' @param col     DEM color palette. DEFAULT: \code{\link{terrain.colors}(100)}
-#' @param nriv    Number of colors for default river palette. A higher number
-#'                means more river stretches will be displayed. DEFAULT: 150
-#' @param rivcol  River color palette. DEFAULT: c(NA, seqPal(nriv, ---))
-#' @param pch,cex Point type + size for colPoints if \code{dem$proj} != NA.
+#' @param rivcol  River color palette. NA will be prepended if rasterriv=TRUE.
+#'                DEFAULT: \code{\link{seqPal}(150, colors=c("lightblue","darkblue"))}
+#' @param rasterriv Logical: should river be added as raster (with \code{\link{vis_asc}})
+#'                instead of lines (with \code{\link{vis_river}})?
+#'                Useful to examine single cells exactly. DEFAULT: FALSE
+#' @param pch,cex Point type + size for \code{\link{colPoints}}.
+#'                Only used if \code{dem$proj} != NA.
 #'                Can each be two values (DEM + FACC separately).
 #'                DEFAULT: pch=15, cex=c(0.14, 0.25)
 #' @param add     Logical: add to existing plot? DEFAULT: FALSE
 #' @param legend  Logical: add \code{\link{colPointsLegend}}? DEFAULT: TRUE
-#' @param title   CHaracter string for legend title. DEFAULT: "Elevation"
-#' @param \dots   Further arguments passed to \code{\link{vis_asc}}.
+#' @param title   Character string for legend title. DEFAULT: "Elevation"
+#' @param \dots   Further arguments passed to \code{\link{vis_asc}} for DEM and
+#'                \code{\link{vis_asc}} or \code{\link{vis_river}} for FACC.
 #'
 vis_dem <- function(
  dem,
  proj=NA,
  col=terrain.colors(100),
- nriv=150,
- rivcol=c(NA, berryFunctions::seqPal(nriv, colors=c("lightblue","darkblue"))),
+ rivcol=berryFunctions::seqPal(150, colors=c("lightblue","darkblue")),
+ rasterriv=FALSE,
  pch=15,
  cex=c(0.14, 0.25),
  add=FALSE,
@@ -45,7 +50,7 @@ if(!is.list(dem)) dem <- read_dem(dem,proj)
 # check list elements:
 check_list_elements(dem, "dem","facc","x","y","file","name","proj")
 if("asc" %in% names(dem)) stop("dem may not contain a list element named asc.")
-# Object element preparation:
+# Plotting object element preparation:
 pdem <- facc <- dem
 names(pdem)[names(pdem)== "dem"] <- "asc"
 names(facc)[names(facc)=="facc"] <- "asc"
@@ -53,8 +58,13 @@ names(facc)[names(facc)=="facc"] <- "asc"
 pch <- rep(pch, length.out=2)
 cex <- rep(cex, length.out=2)
 # Plot:
-     vis_asc(pdem, closedev=FALSE, pch=pch[1], cex=cex[1], add=add,  legend=legend, col=col, title=title, ...)
-o <- vis_asc(facc, closedev=FALSE, pch=pch[2], cex=cex[2], add=TRUE, legend=FALSE, col=rivcol, ...)
+o <- vis_asc(pdem, closedev=FALSE, pch=pch[1], cex=cex[1], add=add,
+             legend=legend, col=col, title=title, ...)
+if(rasterriv)
+  vis_asc(facc, closedev=FALSE, pch=pch[2], cex=cex[2], add=TRUE,
+          legend=FALSE, col=c(NA,rivcol), ...)
+    else
+     vis_river(dem, add=TRUE, legend=FALSE, ...)
 # Turn off device if needed:
 if(o$pdf|o$png) if(!add) dev.off()
 return(invisible(dem))
